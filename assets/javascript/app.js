f77ef3b386699c12c55eadaa9dbb0e07e4a8d16f
Villejoint <kvillejo@estee.com>
2017-05-12 16:35:16 -0400

    Initial commit of files

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+//Create timer at top of page. Set for 30 seconds. 
+	setTimeOut (timeUp, 1000 * 30);
+	console.log(timeUp);
+//function when timer reaches Zero, end game, show results, (and reset?)
+
+//Set rules so that only one answer can be selected at one time.
+//array to hold answer, if userGuess == answer then increase counter
+
+
+//If userGuess !== answer then increase loss counter
+//add these counters to screen at end of round
+
+
+//Function to record right and wrong answers
+
+
+//Add answers to array for counters
+
+//Have counters display when timer reaches zero (or when all questions answered?)
+
+
+});
